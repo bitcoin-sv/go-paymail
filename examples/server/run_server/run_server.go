ef02f53d8eefdb6f8c96509e4f4477ad135242b9
Krzysztof Tomecki <152964795+chris-4chain@users.noreply.github.com>
2024-01-19 08:18:48 +0100

    chore(BUX-497): example with custom capabilities

--- examples/server/run_server/run_server.go
+++ examples/server/run_server/run_server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/bitcoin-sv/go-paymail/logging"
 
 	"github.com/bitcoin-sv/go-paymail/server"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -20,13 +24,14 @@ func main() {
 	config, err := server.NewConfig(
 		new(demoServiceProvider),
 		server.WithBasicRoutes(),
-		server.WithDomain("localhost"), // todo: make this work locally?
+		server.WithDomain("localhost"),
 		server.WithDomain("another.com"),
 		server.WithDomain("test.com"),
 		server.WithGenericCapabilities(),
 		server.WithPort(3000),
 		server.WithServiceName("BsvAliasCustom"),
 		server.WithTimeout(15*time.Second),
+		server.WithCapabilities(customCapabilities()),
 	)
 	config.Prefix = "http://"
 	if err != nil {
@@ -35,4 +40,30 @@ func main() {
 
 	// Create & start the server
 	server.StartServer(server.CreateServer(config), config.Logger)
+}
+
+func customCapabilities() map[string]any {
+	exampleBrfcKey := "406cef0ae2d6"
+	return map[string]any{
+		"custom_static_cap_boolean": false,
+		"custom_static_int":         10,
+		exampleBrfcKey:              true,
+		"custom_callable_cap": server.CallableCapability{
+			Path:   fmt.Sprintf("/display_paymail/%s", server.PaymailAddressTemplate),
+			Method: http.MethodGet,
+			Handler: func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+				incomingPaymail := p.ByName(server.PaymailAddressParamName)
+
+				response := map[string]string{
+					"paymail": incomingPaymail,
+				}
+
+				// Set the content type to application/json
+				w.Header().Set("Content-Type", "application/json")
+
+				// Write the JSON response
+				json.NewEncoder(w).Encode(response)
+			},
+		},
+	}
 }
