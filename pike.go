328c8ec8f647fc06ab726beeee1db39aba58fa97
arkadiuszos4chain <arkadiusz.osowski@4chain.studio>
2024-03-05 11:54:14 +0100

    feat(BUX-395): adjust to review

--- pike.go
+++ pike.go
@@ -37,7 +37,6 @@ func (c *Client) AddContactRequest(url, alias, domain string, request *PikeConta
 	}
 
 	if response.StatusCode != http.StatusOK && response.StatusCode != http.StatusCreated {
-		// Paymail address not found?
 		if response.StatusCode == http.StatusNotFound {
 			err = errors.New("paymail address not found")
 		} else {
@@ -51,28 +50,27 @@ func (c *Client) AddContactRequest(url, alias, domain string, request *PikeConta
 }
 
 func (c *Client) validateUrlWithPaymail(url, alias, domain string) error {
-	if len(url) == 0 || !strings.Contains(url, "https://") {
+	if len(url) == 0 || !strings.HasPrefix(url, "https://") {
 		return fmt.Errorf("invalid url: %s", url)
-	} else if len(alias) == 0 {
+	} else if alias == "" {
 		return errors.New("missing alias")
-	} else if len(domain) == 0 {
+	} else if domain == "" {
 		return errors.New("missing domain")
 	}
 	return nil
 }
 
-func (c *Client) prepareServerErrorResponse(response *StandardResponse) (err error) {
+func (c *Client) prepareServerErrorResponse(response *StandardResponse) error {
+	var details string
+
 	serverError := &ServerError{}
-	if err = json.Unmarshal(response.Body, serverError); err != nil {
-		return
-	}
-	if len(serverError.Message) == 0 {
-		err = fmt.Errorf("bad response from paymail provider: code %d, body: %s", response.StatusCode, string(response.Body))
+	if err := json.Unmarshal(response.Body, serverError); err != nil || serverError.Message == "" {
+		details = fmt.Sprintf("body: %s", string(response.Body))
 	} else {
-		err = fmt.Errorf("bad response from paymail provider: code %d, message: %s", response.StatusCode, serverError.Message)
+		details = fmt.Sprintf("message: %s", serverError.Message)
 	}
 
-	return
+	return fmt.Errorf("bad response from paymail provider: code %d, %s", response.StatusCode, details)
 }
 
 func (r *PikeContactRequestPayload) validate() error {
